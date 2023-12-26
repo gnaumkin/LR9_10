4c087429a7f041631692dee76e1c4c6a8f605315
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-12-26 15:58:27 +0300

    z4.pas

--- z4.pas
+++ z4.pas
@@ -1,48 +1,43 @@
-﻿program z4;
-const
-  size = 5;
+program z4;
+var a, b: array of integer;  // глобальные, формальные
+i, sum1, sum2: integer; // локальные, формальные 
+procedure mas(i, sum1, sum2: integer; var a, b: array of integer); // фактические
+const size = 5;
 var
-  array1: array[1..size] of Integer;
-  array2: array[1..size] of Integer;
-  sum1, sum2, i: Integer; // фактические, глобальные, по ссылке
+  ae,be: array [1..size] of integer; // локальные
 begin
-  Randomize;
+  // создание и вывод начальных массивов
+  writeln('Начальные 2  массива:');
   for i := 1 to size do
   begin
-    array1[i] := Random(21) - 10;
-    array2[i] := Random(21) - 10;
+    ae[i] := random(21) - 10;
+    be[i] := random(21) - 10;
   end;
-  WriteLn('Массив 1:');
+  writeln(ae);
+  writeln();
+  writeln(be);
+  writeln;
+  // вычисление сумм положительных элементов в каждом массиве
   for i := 1 to size do
-    WriteLn(array1[i]);
-  WriteLn('Массив 2:');
-  for i := 1 to size do
-    WriteLn(array2[i]);
-  sum1 := 0;  // локальные
-  sum2 := 0;
-  for i := 1 to SIZE do
   begin
-    if array1[i] > 0 then
-      sum1 := sum1 + array1[i];
-    if array2[i] > 0 then
-      sum2 := sum2 + array2[i];
+    if ae[i] > 0 then
+      sum1 := sum1 + ae[i];
+    if be[i] > 0 then
+      sum2 := sum2 + be[i];
   end;
+  // умножение на 10 элементов массива, у которого сумма положительных элементов меньше
   if sum1 < sum2 then
-  begin
-    WriteLn('Умножаем элементы 1 массива на 10:');
     for i := 1 to size do
-    begin
-      array1[i] := array1[i] * 10;
-      WriteLn(array1[i]);
-    end;
-  end
+      ae[i] := ae[i] * 10
   else
-  begin
-    WriteLn('Умножаем элементы 2 массива на 10:');
-    for i := 1 to SIZE do
-    begin
-      array2[i] := array2[i] * 10;
-      WriteLn(array2[i]); // фактические
-    end;
-  end;
+    for i := 1 to size do
+      be[i] := be[i] * 10;
+  // вывод изменённых массивов
+  writeln('Изменённые 2 массива:');
+  writeln(ae);
+  writeln();
+  writeln(be);
+end;
+begin
+  mas(i, sum1, sum2, a, b);
 end.
