727dde751a74f2e8c5741fbd4ab6af4ca9fb82ac
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-12-26 14:34:11 +0300

    z3.pas

--- z3.pas
+++ z3.pas
@@ -1,18 +1,18 @@
 ﻿program z3;
 var
   arr: array[1..10] of Integer;
-  lastnegi, i: Integer;
+  lastnegi, i: Integer; // фактические, глобальные, по ссылке
 begin
   arr[1] := 2;
   arr[2] := -5;
   arr[3] := 10;
   arr[4] := -8;
   arr[5] := 3;
-  lastnegi := 0;
+  lastnegi := 0; // локальные
   for i := 1 to 10 do
   begin
     if arr[i] < 0 then
       lastnegi := i;
   end;
-  WriteLn('Последний отрицательный элемент: ', lastnegi);
+  WriteLn('Последний отрицательный элемент: ', lastnegi); // фактические
 end.
